f4f866cb2a3e718b5cf8237654ae3dd7ad4dbd7b
cancioja <cancioja@gmail.com>
2022-08-12 23:55:15 -0300

    Se crea el script blankLinesCounter.sh del Ejercicio 5, se prueba con el texto. OK

---
+++ blankLinesCounter.sh
@@ -0,0 +1,11 @@
+clear
+echo "---------------------------------------------------"
+echo "        Contador de líneas en blanco)"
+echo "---------------------------------------------------"
+echo "                 EJERCICIO  5"
+echo "---------------------------------------------------"
+
+# ^$ es una expresión regular que sólo coincide con una línea en blanco (un inicio de línea seguido de un final de línea)
+
+echo "La Cantidad de líneas en blanco en el texto es: $(cat Textoprueba.txt|grep "^$"|wc -l)"
+echo
